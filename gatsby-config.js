cf41cede989734373acc9fc71a2e95a4c1d69eba
Drew <drewclementsdesign@gmail.com>
2020-02-23 13:35:21 -0500

    prefetch google fonts for mobile

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,17 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Kanit`,
+            variants: [`400i`, `500i`, `900i`]
+          }
+        ]
+      }
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-postcss`,
