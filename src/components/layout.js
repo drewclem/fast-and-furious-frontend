0bb78b221b572400d035c976686e4fbe79f2c4db
Drew <drewclementsdesign@gmail.com>
2020-02-23 13:44:24 -0500

    relearning google fonts for gatsby lol

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "../components/fnf.css"
+import "../static/fnf.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
